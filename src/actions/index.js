027fa73c350424e1ad7a9d0c8d49194c11bf5f94
정용채 <asd0905@naver.com>
2021-05-23 16:30:29 +0900

    feat: action, container, app.js setting

---
+++ src/actions/index.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const API_KEY = '1789c77dd563f3737155d4f15db0b8c0';
+const ROOT_URL = `//api.openweathermap.org/data/2.5/forecast?appid=${API_KEY }`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},kr`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+    // 만약 페이로드가 프로미스면, 리덕스 프로미스는 액션 전체를 멈춤
+    // 프로미스가 풀어지면 다시 재가동
+    // 비동기적 코딩할때처럼 코드가 길어질 필요가 없다
+}
---
+++ src/containers/search_bar.js
@@ -0,0 +1,60 @@
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import {bindActionCreators} from "redux";
+import {fetchWeather} from "../actions";
+
+class SearchBar extends Component {
+    constructor(pros) {
+        super(pros);
+
+        this.state = {term: ''};
+
+        // this.setState에러를 해결하기 위해서
+        this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+    }
+
+    onInputChange(event) {
+        // 이대로 실행하면 setState를 찾을 수 없다는 에러가 발생
+        this.setState({term: event.target.value});
+    }
+
+    /*onInputChange = (event) => {
+        console.log(event.target.value);
+        this.setState({term: event.target.value});
+    }*/
+
+    onFormSubmit(event) {
+        event.preventDefault();
+
+        // 날씨 데이터를 가져와야 한다.
+        this.props.fetchWeather(this.state.term);
+        this.setState({term: ''});
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.onFormSubmit} className="input-group">
+                <input
+                    type="text"
+                    placeholder="당신이 좋아하는 도시의 5일간의 날씨를 알아보세요"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange} />
+                <span className="input-group-btn">
+                    <button type="submit" className="btn btn-secondary">Submit</button>
+                </span>
+            </form>
+        )
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    // 액션 생성장 fetchWeather를 this.props에 넣어주고 이를 실행할 때마다
+    // dispatch와 함께 액션이 반환된다.
+    // dispatch는 급파한다는 의미를 가지고 있듯이 액션을 전체 리듀서에게 보내준다.
+    return bindActionCreators({fetchWeather}, dispatch);
+}
+
+// param 순서 (글로벌 어플리케이션 스테이트를 props로 맵핑해주는 함수, 액션생성자를 props로 맵핑해주는 함수)
+export default connect(null, mapDispatchToProps)(SearchBar);
